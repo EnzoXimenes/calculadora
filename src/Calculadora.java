8ab1846655fe944275267eb864d6714275ffd12d
Enzo Ximenes <enzoximenes89@gmail.com>
2026-05-15 14:55:56 -0300

    first commit

---
+++ src/Calculadora.java
@@ -0,0 +1,10 @@
+public class Calculadora {
+    int a = 10;
+    int b = 20;
+    public static void somarMultiplicar(int a,int b) {
+        
+        System.out.println("A soma de " + a + "+" + b + "é igual a: " + (a+b)); 
+        System.out.println("A multiplicação de " + a + "+" + b + "é igual a: " + (a*b));
+    }
+    }
+
