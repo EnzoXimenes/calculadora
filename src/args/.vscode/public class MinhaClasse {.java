ef49abd9730a82e6f07e4e61d43f9687ddae7d7e
Enzo Ximenes <enzoximenes89@gmail.com>
2026-05-15 18:30:25 -0300

    Versão 2.0 BANCO VERSÃO 1.0

---
+++ src/args/.vscode/public class MinhaClasse {.java
@@ -0,0 +1,12 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String nome = agrs [0]
+        String sobrenome = args[1]; 
+        double altura = Double.valueOf(args[2]);   
+        int idade = Integer.valueOf(args[3]);           
+        
+        System.out.println("Olá, " + nome + " " + sobrenome + "!");
+        System.out.println("Sua altura é: " + altura);
+        System.out.println("Sua idade é: " + idade);
+    }
+}
